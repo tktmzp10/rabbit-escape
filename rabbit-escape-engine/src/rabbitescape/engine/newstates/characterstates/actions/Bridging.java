c19db09e43a67f0e80e06f2b15ef7484935a2582
DaOnPark <deeopark39@gmail.com>
2019-06-06 22:58:51 +0900

    Adding state/strategy pattern to Bridging v9
    Part of a process to remove switch-case statements to reduce cyclomatic complexity. This step reverts behave() method in Bridging back to the original.

--- rabbit-escape-engine/src/rabbitescape/engine/newstates/characterstates/actions/Bridging.java
+++ rabbit-escape-engine/src/rabbitescape/engine/newstates/characterstates/actions/Bridging.java
@@ -1,5 +1,6 @@
 package rabbitescape.engine.newstates.characterstates.actions;
 
+import static rabbitescape.engine.Block.Material.EARTH;
 import static rabbitescape.engine.Direction.RIGHT;
 import static rabbitescape.engine.things.items.ItemType.*;
 import static rabbitescape.engine.Block.Shape.*;
@@ -503,7 +504,171 @@ public class Bridging extends CharacterActionStates
     @Override
     public boolean behave( World world, Character character, State state )
     {
-        return bridgingState.behave( world, character, bridgeType );
+        switch ( state )
+        {
+            case RABBIT_BRIDGING_RIGHT_1:
+            case RABBIT_BRIDGING_RIGHT_2:
+            case RABBIT_BRIDGING_LEFT_1:
+            case RABBIT_BRIDGING_LEFT_2:
+            {
+                bridgeType = BridgeType.ALONG;
+                return true;
+            }
+            case RABBIT_BRIDGING_UP_RIGHT_1:
+            case RABBIT_BRIDGING_UP_RIGHT_2:
+            case RABBIT_BRIDGING_UP_LEFT_1:
+            case RABBIT_BRIDGING_UP_LEFT_2:
+            {
+                bridgeType = BridgeType.UP;
+                return true;
+            }
+            case RABBIT_BRIDGING_DOWN_UP_RIGHT_1:
+            case RABBIT_BRIDGING_DOWN_UP_RIGHT_2:
+            case RABBIT_BRIDGING_DOWN_UP_LEFT_1:
+            case RABBIT_BRIDGING_DOWN_UP_LEFT_2:
+            {
+                bridgeType = BridgeType.DOWN_UP;
+                return true;
+            }
+            case RABBIT_BRIDGING_IN_CORNER_RIGHT_1:
+            case RABBIT_BRIDGING_IN_CORNER_LEFT_1:
+            case RABBIT_BRIDGING_IN_CORNER_RIGHT_2:
+            case RABBIT_BRIDGING_IN_CORNER_LEFT_2:
+            case RABBIT_BRIDGING_IN_CORNER_UP_RIGHT_1:
+            case RABBIT_BRIDGING_IN_CORNER_UP_LEFT_1:
+            case RABBIT_BRIDGING_IN_CORNER_UP_RIGHT_2:
+            case RABBIT_BRIDGING_IN_CORNER_UP_LEFT_2:
+            {
+                bridgeType = BridgeType.ALONG;
+                return true;
+            }
+            case RABBIT_BRIDGING_RIGHT_3:
+            case RABBIT_BRIDGING_DOWN_UP_RIGHT_3:
+            {
+                character.x++;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_RIGHT,
+                        0
+                    )
+                );
+
+                return true;
+            }
+            case RABBIT_BRIDGING_LEFT_3:
+            case RABBIT_BRIDGING_DOWN_UP_LEFT_3:
+            {
+                character.x--;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_LEFT,
+                        0
+                    )
+                );
+
+                return true;
+            }
+            case RABBIT_BRIDGING_UP_RIGHT_3:
+            {
+                character.x++;
+                character.y--;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_RIGHT,
+                        0
+                    )
+                );
+
+                return true;
+            }
+            case RABBIT_BRIDGING_UP_LEFT_3:
+            {
+                character.x--;
+                character.y--;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_LEFT,
+                        0
+                    )
+                );
+
+                return true;
+            }
+            case RABBIT_BRIDGING_IN_CORNER_RIGHT_3:
+            {
+                character.onSlope = true;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_RIGHT,
+                        0
+                    )
+                );
+                return true;
+            }
+            case RABBIT_BRIDGING_IN_CORNER_LEFT_3:
+            {
+                character.onSlope = true;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_LEFT,
+                        0
+                    )
+                );
+                return true;
+            }
+            case RABBIT_BRIDGING_IN_CORNER_UP_RIGHT_3:
+            {
+                character.onSlope = true;
+                character.y--;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_RIGHT,
+                        0
+                    )
+                );
+                return true;
+            }
+            case RABBIT_BRIDGING_IN_CORNER_UP_LEFT_3:
+            {
+                character.onSlope = true;
+                character.y--;
+                world.changes.addBlock(
+                    new Block(
+                        character.x,
+                        character.y,
+                        EARTH,
+                        BRIDGE_UP_LEFT,
+                        0
+                    )
+                );
+                return true;
+            }
+            default:
+            {
+                return false;
+            }
+        }
     }
 
     @Override
