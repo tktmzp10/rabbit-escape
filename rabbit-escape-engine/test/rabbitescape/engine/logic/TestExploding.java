a3d82c7b546522252ab442d79c0d46856e380981
jkuot36 <jkuot36@naver.com>
2019-06-12 00:24:47 +0900

    Exploding Test case addition      * Purpose: To test rabbit explodes when picks up explode item.      * Input:      * World maps instances that rabbit picks up explode item.      * Expected: Rabbit instance is to be eliminated.

--- rabbit-escape-engine/test/rabbitescape/engine/logic/TestExploding.java
+++ rabbit-escape-engine/test/rabbitescape/engine/logic/TestExploding.java
@@ -6,6 +6,12 @@ import org.junit.Test;
 
 public class TestExploding {
 
+    /**
+     * Purpose: To test rabbit explodes when picks up explode item.
+     * Input:
+     * World maps instances that rabbit picks up explode item.
+     * Expected: Rabbit instance is to be eliminated.
+     */
     @Test
     public void Rabbit_explodes_when_picks_up_item() {
         assertWorldEvolvesLike(
